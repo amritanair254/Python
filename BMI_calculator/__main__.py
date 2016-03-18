7d34fce8f2bf318325199552784b84e27694403f
Amrita Nair <amritanair254@gmail.com>
2016-03-18 19:54:44 +0530

    Update __main__.py

--- BMI_calculator/__main__.py
+++ BMI_calculator/__main__.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python
-import health,re,os
+import Health_lib,re,os
 
 
 def main():
-	time=str(health.datetime.now())[:19]
+	time=str(Health_lib.datetime.now())[:19]
 	if os.path.isfile("bmi.txt") :
 	    os.remove("bmi.txt")
 	name = raw_input("Name > ")
-	health.printer("\nBMI report of "+name+" on "+time)
+	Health_lib.printer("\nBMI report of "+name+" on "+time)
 	height = raw_input("Height in feet and inches or centimeters. Eg: '5ft 9in' or '182cm' > ")
 	if re.match(".*cm.*",height):
 		height = float(height.split('cm')[0])/2.54		#everything in inches
@@ -26,12 +26,12 @@ def main():
 	else:
 		sex="female"
 	age=int(raw_input("Age > "))
-	health.printer("Height:"+str(int(height/12))+"ft "+str(int(height%12))+"in | Weight:"+str(weight)+"lbs")
+	Health_lib.printer("Height:"+str(int(height/12))+"ft "+str(int(height%12))+"in | Weight:"+str(weight)+"lbs")
 	
 	
-	bmi=health.bmi_func(weight,height)
-	bmr=health.basal_metabolic_rate(sex,age,weight,height)
-	cal=health.calorie_needs(sex,bmr)
+	bmi=Health_lib.bmi_func(weight,height)
+	bmr=Health_lib.basal_metabolic_rate(sex,age,weight,height)
+	cal=Health_lib.calorie_needs(sex,bmr)
 	
 	result=raw_input("\nDo you want to find out your body fat percentage/ratios/indices? 'y' or 'n' > ")
 	if result!="n":
@@ -55,13 +55,13 @@ def main():
 		if re.match(".*cm.*",forearm):
 			forearm = float(forearm.split('cm')[0])/2.54
 		else:	forearm = float(forearm.split('in')[0])
-		health.printer("Chest:"+str(chest)+"in| Waist:"+str(waist)+"in| Hip:"+str(hip)+"in| Forearm:"+str(forearm)+"in| Wrist:"+str(wrist)+"in\n")
-		lbm,bf=health.body_fat(weight,sex,wrist,waist,hip,forearm)
-		bai=health.body_adipose_index(height,hip,age,sex)
-		wht=health.waist_to_height_ratio(waist, height, sex)
-		whr=health.waist_to_hip_ratio(waist, hip, sex)
+		Health_lib.printer("Chest:"+str(chest)+"in| Waist:"+str(waist)+"in| Hip:"+str(hip)+"in| Forearm:"+str(forearm)+"in| Wrist:"+str(wrist)+"in\n")
+		lbm,bf=Health_lib.body_fat(weight,sex,wrist,waist,hip,forearm)
+		bai=Health_lib.body_adipose_index(height,hip,age,sex)
+		wht=Health_lib.waist_to_height_ratio(waist, height, sex)
+		whr=Health_lib.waist_to_hip_ratio(waist, hip, sex)
 		if sex=="male":
-			pig=health.pignet_index(chest,weight,height)
+			pig=Health_lib.pignet_index(chest,weight,height)
 		else :  pig=''
 	else:
 		wrist=''
@@ -78,11 +78,11 @@ def main():
 	
 	rxr=raw_input("\nEnter email if you want to us to maintain your record and send you your report via email > ")
 	if rxr!='':
-		health.maintain_record(name,time,age,sex,height,weight,bmi,bmr,cal,wrist,forearm,chest,waist,hip,lbm,bf,bai,wht,whr,pig,rxr)
+		Health_lib.maintain_record(name,time,age,sex,height,weight,bmi,bmr,cal,wrist,forearm,chest,waist,hip,lbm,bf,bai,wht,whr,pig,rxr)
 		history=raw_input("Do you want to check your history of measurements? (Doesn't apply to new users) > ")
 		if history!="n" :
-			health.past_records(rxr)
-		health.send_mail(rxr)
+			Health_lib.past_records(rxr)
+		Health_lib.send_mail(rxr)
 
 if __name__=='__main__':
 	main()
