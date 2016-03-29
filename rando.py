769e17507075d9dbac980eecad30b4e14bb7fa96
Amrita Nair <amritanair254@gmail.com>
2016-03-29 14:39:16 +0530

    Update rando.py

--- rando.py
+++ rando.py
@@ -1,8 +1,5 @@
-import os
-import sys
-import getopt
-import string
-import sqlite3
+#! /usr/bin/env python
+import os,sys,getopt,string,sqlite3
 limit=10
 
 def main():
@@ -10,7 +7,7 @@ def main():
     col2=""
     col3=""
     col4=""
-    (options, args) = getopt.getopt(sys.argv[1:], "d1:c1:d2:c2:", ["col1=","col2=","col3=","col4="])
+    (options, args) = getopt.getopt(sys.argv[1:], "c1:c2:c3,c4:", ["col1=","col2=","col3=","col4="])
     for (name, value) in options:
         if name in ("--col1","-c1"):
             col1=value
@@ -31,11 +28,11 @@ def main():
     if len(col1)>0:
         colz(col1,c)
     if len(col2)>0:
-        colz(col2)
+        colz(col2,c)
     i=raw_input("Do you want table to be printed to screen? y or n? >")
-    if i=="y":
-        t = conn.execute("SELECT * from bmi")
+    if i!="n":
         printer(t)
+    conn.close()
 
 
 def colz(col,c):
@@ -61,16 +58,9 @@ def colz(col,c):
 
 
 def printer(c):
-        t = PrettyTable(['Date','Height in.','Weight lb.','Wrist in.','Forearm in.','Chest in.','Waist in.','Hip in.','BMI'])
-        y = PrettyTable(['Date','Metabolic Rate','Lean Mass lb.','Body Fat %','Body Fat Ind.','Waist/Height','Waist/Hip','Pignet Ind.'])
-        for row in c:
-            if row[20] == rxr:
-                t.add_row([row[1],row[4],row[5],row[9],row[10],row[11],row[12],row[13],row[6]])
-                y.add_row([row[1],row[7],row[14],row[15],row[16],row[17],row[18],row[19]])
-        print t
-        print "\n"
-        print y
-        conn.close()
+    c.execute('''SELECT * FROM rando''')
+    mytable = prettytable.from_db_cursor(cursor)
+    print mytable
 
 
 if __name__ == "__main__":
